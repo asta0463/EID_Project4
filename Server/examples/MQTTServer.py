91e248aa34123d1d34e45180faa76af1bbfa3e92
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-12-11 12:45:30 +0530

    Update MQTTServer.py

--- Server/examples/MQTTServer.py
+++ Server/examples/MQTTServer.py
@@ -2,6 +2,7 @@
 ##  @brief: Implementation of Server Side MQTT
 ##  @Authors: Rhea Cooper, Ashish Tak (Univeristy of Colorado, Boulder)
 ##  @Date: 12/11/2017
+## Reference: http://www.steves-internet-guide.com/into-mqtt-python-client/
 
 
 import paho.mqtt.client as mqtt
@@ -23,16 +24,20 @@ def on_message(client, userdata, msg):
   #print("message qos=",msg.qos)
   #print("message retain flag=",msg.retain)
   #print("Publishing message to topic")
+  
+  #publish to a different topic
   client.publish("P4",str(msg.payload.decode("utf-8")))
   
 
 def on_subscribe(client, userdata, mid, granted_qos):
   print("sub ack " + str(mid) + "qos " +str(granted_qos))
-    
+ 
+#connecting to an online broker
 client = mqtt.Client()
 broker_address="iot.eclipse.org"
 client.connect(broker_address)
 
+#Attaching the defined methods to the object
 client.on_connect = on_connect
 client.on_subscribe = on_subscribe
 client.on_message = on_message
